33f28c97f7ea6c93af148223777b41580f4a61bd
iralitvinchuk <47001760+iralitv@users.noreply.github.com>
2021-04-12 13:28:45 +0300

    feat: to do app complete

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, FlatList } from 'react-native';
+import GoalInput from './components/GoalInput';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+
+  console.log(courseGoals)
+
+  const addGoalHandler = (goalTitle) => {
+    if (goalTitle.length === 0) {
+      return;
+    }
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { key: Math.random().toString(), value: goalTitle }
+    ]);
+    setIsAddMode(false);
+  };
+
+  const removeGoalHandler = (goalId) => {
+    setCourseGoals(currentGoals => currentGoals.filter(goal => goal.key !== goalId));
+  };
+
+  const showAddModeHandler = () => {
+    setIsAddMode(true);
+  };
+
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Button title="Add New Goal" onPress={showAddModeHandler} />
+      <GoalInput
+        visible={isAddMode}
+        onAddGoal={addGoalHandler} 
+        onCancel={cancelGoalAdditionHandler} />
+      <FlatList
+        data={courseGoals}
+        renderItem={itemData =>
+          <GoalItem 
+            onDelete={removeGoalHandler} 
+            id={itemData.item.key} 
+            title={itemData.item.value} 
+          />
+        }
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
   },
 });
